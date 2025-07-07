c844032cc26feb0e5d045c22e7ffd8f02107dd52
eeekeee <ekrtionl@gmail.com>
2025-07-07 14:10:18 +0900

    feat: add basic tray steup

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,6 +2,7 @@ import { app, BrowserWindow } from "electron";
 import { ipcMainHandle, isDev } from "./util.js";
 import { getStaticData, pollResources } from "./resourceManager.js";
 import { getPreloadPath, getUIPath } from "./pathResolver.js";
+import { createTray } from "./tray.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
@@ -21,4 +22,36 @@ app.on("ready", () => {
   ipcMainHandle("getStaticData", () => {
     return getStaticData();
   });
+
+  createTray(mainWindow);
+
+  handleCloseEvents(mainWindow);
 });
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+  let willClose = false;
+
+  mainWindow.on("close", (event) => {
+    if (willClose) {
+      return;
+    }
+
+    // 종료 버튼을 눌러도 백그라운드에서 계속 실행됨
+    event.preventDefault();
+    mainWindow.hide();
+
+    // macOS에서는 dock 아이콘을 숨김
+    if (app.dock) {
+      app.dock.hide();
+    }
+  });
+
+  // 앱이 종료될 때 이벤트 핸들러를 등록
+  app.on("before-quit", () => {
+    willClose = true;
+  });
+
+  mainWindow.on("show", () => {
+    willClose = false;
+  });
+}
